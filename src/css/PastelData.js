edd680a133a90188b4e3d433c6833e85c39bdfd9
mikironari <119760829+mikironari@users.noreply.github.com>
2024-04-13 23:31:31 +0900

    Add files via upload

---
+++ src/css/PastelData.js
@@ -0,0 +1,55 @@
+let PastelData = [
+
+  {
+    id : 0,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_360,c_limit/a6cb5fd1-fe7a-4b0a-83a2-a662fcc64393/acg-%EB%82%A8%EC%84%B1-%EB%93%9C%EB%9D%BC%EC%9D%B4-%ED%95%8F-%ED%8B%B0%EC%85%94%EC%B8%A0-Jx3vSPy6.png",
+    title : "나이키 ACG",
+    content: "드라이 핏 티셔츠",
+    price : "59,000 원"
+  },
+
+  {
+    id : 1,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_360,c_limit/0bcb4233-88f5-4e2c-978e-5b8ff3c765b2/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%EB%82%A8%EC%84%B1-%EC%98%A4%EB%B2%84%EC%82%AC%EC%9D%B4%EC%A6%88-%ED%8B%B0%EC%85%94%EC%B8%A0-H4NdsUlX.png",
+    title : "나이키 스포츠웨어",
+    content: "오버사이즈 티셔츠",
+    price : "65,000 원"
+  },
+  {
+    id : 2,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_398,c_limit/e20ee375-d732-4874-8b39-2bb5d2f32186/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%ED%94%84%EB%A6%AC%EB%AF%B8%EC%97%84-%EC%97%90%EC%84%BC%EC%85%9C-%EB%82%A8%EC%84%B1-%ED%8B%B0%EC%85%94%EC%B8%A0-kxhNK9v6.png",
+    title : "나이키 프리미엄 에센셜",
+    content: "남성 티셔츠",
+    price : "55,000 원"
+  },
+  {
+    id : 3,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_398,c_limit/dcf06353-6cb3-43e8-bfdb-67f3af521923/%EC%86%94%EB%A1%9C-%EC%8A%A4%EC%9A%B0%EC%8B%9C-%EB%82%A8%EC%84%B1-%ED%94%8C%EB%A6%AC%EC%8A%A4-%ED%81%AC%EB%A3%A8-xn5FjzNR.png",
+    title : "나이키 솔로 스우시",
+    content: "남성 플리스 크루",
+    price : "119,000 원"
+  },
+  {
+    id : 4,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_398,c_limit/2c104413-1c62-4d78-a1d6-c070e387596a/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%EC%97%90%EC%84%BC%EC%85%9C-%EC%97%AC%EC%84%B1-%EB%B0%95%EC%8B%9C-%ED%8B%B0%EC%85%94%EC%B8%A0-YDJePold.png",
+    title : "나이키 스포츠웨어",
+    content: "박시 티셔츠",
+    price : "49,000 원"
+  },
+  {
+    id : 5,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_398,c_limit/0a2c54f2-34a1-47c6-9096-11c52b6c9d82/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%EC%97%90%EC%84%BC%EC%85%9C-%EC%9C%88%EB%93%9C%EB%9F%AC%EB%84%88-%EC%97%AC%EC%84%B1-%EC%9A%B0%EB%B8%90-%EC%9E%AC%ED%82%B7-wcxM8iTo.png",
+    title : "나이키 윈드러너",
+    content: "여성 우븐 재킷",
+    price : "135,000 원"
+  },
+  {
+    id : 6,
+    img : "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_2.0/h_398,c_limit/db002fa8-5871-4eb6-ba18-ef9cd9697313/%EC%97%90%EC%96%B4-%EC%97%AC%EC%84%B1-%EC%98%A4%EB%B2%84%EC%82%AC%EC%9D%B4%EC%A6%88-%EC%9A%B0%EB%B8%90-%EB%B4%84%EB%B2%84-%EC%9E%AC%ED%82%B7-KW0I76DQ.png",
+    title : "나이키 에어",
+    content: "오버사이즈 재킷",
+    price : "159,000 원"
+  }
+]
+
+export default PastelData;
---
+++ src/css/PastelSlick.js
@@ -0,0 +1,94 @@
+/*react slick*/
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
+import PastelData from "../data/PastelData";
+
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", right: "0"}}
+      onClick={onClick}
+    />
+  );
+}
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", left: "0"}}
+      onClick={onClick}
+    />
+  );
+}
+
+function PastelSlick() {
+  let [pastelList] = useState(PastelData);
+  
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    centerMode: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <div className="slider-container mb-5">
+      
+      <Slider {...settings}>
+        {
+          pastelList.map((item) => (
+            <div className='main1Swiper mb-3 mt-2'
+            key={item.id}>
+              <div>
+                <img className="mb-3"
+                src={item.img}/>
+              </div>
+              <p className="bold">{item.title}</p>
+              <p>{item.content}</p>
+              <p className="bold">{item.price}</p>
+            </div>
+          ))
+        }
+      </Slider>
+    </div>
+  );
+}
+
+export default PastelSlick;
