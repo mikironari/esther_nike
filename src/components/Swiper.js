50b98dcdfc105465a5ab422a0a422cc972c4cfd7
Esther Kim <119760829+mikironari@users.noreply.github.com>
2024-07-10 16:29:31 +0900

    Add files via upload

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -3,6 +3,8 @@ import SwiperCore, { Navigation, Pagination } from 'swiper';
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+// import required modules
+// import { Pagination } from 'swiper/modules';
 
 
 SwiperCore.use([Navigation, Pagination]);
@@ -12,17 +14,34 @@ const SwiperComponent = ( { children } ) => {
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={2}
       scrollbar={{ draggable: true }}
-      
+      // centeredSlides={false}
       naviagation
-      // pagination={{ clickable: true }}
+      pagination={{ clickable: true }}
+      // modules={[Pagination]}
+      //   className="mySwiper"
       breakpoints={{ 
-        768: {
-          slidesPerView:6,
+        280: {
+          slidesPerView: 1
         },
+        300: {
+          slidesPerView: 2
+        },
+        360: {
+          slidesPerView: 2
+        },
+        530: {
+          slidesPerView: 3
+        },
+        900: {
+          slidesPerView: 4
+        },
+        1200: {
+          slidesPerView: 5
+        },
+        
       }}>
-        {children.map((child, index)=> (
+        {children.map((child, index)=> ( //좌우측에 나타나는 navigation은 children 매개변수로 받은 slice들을 하나씩 순서대로 나오게 작성한 것...
           <SwiperSlide key={index}>{child}</SwiperSlide>
         ))}
       </Swiper>
